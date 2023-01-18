601e230c42ed8d9e54c4bde7e245e6cb44a47a0c
yabu2080 <kidistkifleyohannes364@gmail.com>
2023-01-18 02:12:16 -0800

    agin

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
-"""
-Using https://jsonplaceholder.typicode.com
-gathers data from API and exports it to JSON file
-Implemented using recursion
-"""
-import json
-import re
-import requests
-import sys
+"""Exports data in the JSON format"""
 
+if __name__ == "__main__":
 
-API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
+    import json
+    import requests
+    import sys
 
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    
+    todoUser = {}
+    taskList = []
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('username')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            with open("{}.json".format(id), 'w') as json_file:
-                user_data = list(map(
-                    lambda x: {
-                        "task": x.get("title"),
-                        "completed": x.get("completed"),
-                        "username": user_name
-                        },
-                    todos
-                ))
-                user_data = {
-                        "{}".format(id): user_data
-                    }
-                json.dump(user_data, json_file)
+    for task in todos:
+        if task.get('userId') == int(userId):
+            taskDict = {"task": task.get('title'),
+                    "completed": task.get('completed'),
+                    "username": user.json().get('username')}
+            taskList.append(taskDict)
+    todoUser[userId] = taskList
+
+    filename = userId + '.json'
+    with open(filename, mode='w') as f:
+        json.dump(todoUser, f)
